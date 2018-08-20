867be56cc85b8c09cb713c1cdc89dc1cfbeb1fda
Adam Wilson <awilson@keystonelearning.com>
2018-08-20 10:30:59 -0400

    Copy POC solution into sample repo.

---
+++ Sample.Library/Sample.Library/SampleClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Sample.Library
+{
+	public class SampleClass
+	{
+		public int Add(int first, int second) => first + second;
+		public int Subtract(int minuend, int subtrahend) => minuend - subtrahend;
+		public int Multiply(int multiplicand, int multiplier) => multiplicand * multiplier;
+		public int Divide(int dividend, int divisor) => dividend / divisor;
+
+		public bool ReturnFalse() => false;
+		public bool ReturnTrue() => true;
+		public int Square(int num) => num*num;
+	}
+}
---
+++ Sample.Library/TheSecond.Library/FluxCapacitor.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace TheSecond.Library
+{
+	public static class FluxCapacitor
+	{
+		public static string GetMessage() => "This is heavy, Doc.";
+	}
+}
